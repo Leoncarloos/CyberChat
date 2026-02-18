a49b3c7653de00b650cc0b4fb53ea0da533c6e7d
Leoncarloos <dogeleon72@gmail.com>
2026-02-18 17:27:54 -0500

    fix: use origin for internal fetch

--- lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseBrowser } from "@/lib/supabaseBrowser";
-
-export async function getUserOrNull() {
-  const supabase = supabaseBrowser();
-  const { data } = await supabase.auth.getUser();
-  return data.user ?? null;
-}
